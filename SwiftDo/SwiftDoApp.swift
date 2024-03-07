58f54db925b78f514e1dbe2dc2a238af382466ab
siraajul <shirajulislamparvez@gmail.com>
2024-03-07 23:28:16 +0600

    Initial Commit

---
+++ SwiftDo/SwiftDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDoApp.swift
+//  SwiftDo
+//
+//  Created by Sirajul Islam on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
