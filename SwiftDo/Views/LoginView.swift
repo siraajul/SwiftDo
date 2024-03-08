4e32396ddebf7baf1ef2c38a300d2d7ec64e9cae
siraajul <shirajulislamparvez@gmail.com>
2024-03-08 18:27:35 +0600

    LoginView --header ✅

--- SwiftDo/Views/LoginView.swift
+++ SwiftDo/Views/LoginView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            //Header
+            ZStack{
+                RoundedRectangle(cornerRadius: 0)
+                    .foregroundColor(Color.pink)
+                    .rotationEffect(Angle(degrees: 14))
+                VStack{
+                    Text("SwfitDo")
+                        .font(.system(size: 50))
+                        .foregroundColor(Color.white)
+                        .bold()
+                    Text("Get Things Done !")
+                        .font(.system(size: 30))
+                        .foregroundColor(Color.white)
+                }
+                .padding(.top,10)
+            }
+            .frame(width: UIScreen.main.bounds.width*3, height: 315)
+            .offset(y:-100)
+
+            //Login Form
+            
+            //Create Account
+        Spacer()
+        }
     }
 }
 
